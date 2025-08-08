5eada0af9b76423cfa252d26f85ff79106d4b157
Romel <200869522+rblamug@users.noreply.github.com>
2025-08-08 12:21:16 -0400

    Added remove and status button
    remove should delete the card and status changes
    whether the book as been read or not

--- library.js
+++ library.js
@@ -1,24 +1,30 @@
 const myLibrary = [
     {
         title: 'Jurassic Park',
-        author: 'Michael Crichton',
+        author: 'by Michael Crichton',
         pages: '464 pages',
-        id: 123
+        id: 123,
+        status: 'Read'
     },
     {
         title: 'The Lord of the Rings',
-        author: 'J.R.R. Tolkien',
+        author: ' by J.R.R. Tolkien',
         pages: '1077 pages',
-        id: 345
+        id: 345,
+        status: 'Not Read'
     }
 ];
 
 function Book(title, author, pages, status) {
     this.title = title;
-    this.author = author;
+    this.author = `by ${author}`;
     this.pages = `${pages} pages`;
     this.status = status;
     this.id = crypto.randomUUID();
+}
+
+Book.prototype.toggleStatus = function () {
+    this.status = (this.status === 'Read') ? 'Not Read' : 'Read';
 }
 
 function addBookToLibrary(book) {
@@ -33,21 +39,41 @@ function displayBooks() {
         const bookCard = document.createElement('div');
         library.appendChild(bookCard);
         for (const key in book) {
-            if (key !== 'id') {
+            if (key !== 'id' && key !== 'toggleStatus') {
                 const listItem = document.createElement('li');
                 listItem.textContent = book[key];
                 bookCard.appendChild(listItem);
             }
         }
+        // create remove button
         const removeBtn = document.createElement('button');
         removeBtn.textContent = "Remove";
         removeBtn.setAttribute('data-id', book.id);
         removeBtn.classList.add('removeBtn');
         bookCard.appendChild(removeBtn);
 
+        // create status button
+        const statusBtn = document.createElement('button');
+        statusBtn.textContent = 'Status';
+        statusBtn.setAttribute('data-id', book.id);
+        statusBtn.classList.add('statusBtn');
+        bookCard.appendChild(statusBtn);
+
+        // status button find book id for toggle function
+        document.querySelectorAll('.statusBtn').forEach(button => {
+            button.addEventListener('click', function () {
+                const bookId = this.getAttribute("data-id");
+                const index = myLibrary.findIndex(book => book.id === bookId);
+                myLibrary[index].toggleStatus();
+                displayBooks();
+            });
+        });
+
+        // remove button find book id for remove function
         document.querySelectorAll('.removeBtn').forEach(button => {
             button.addEventListener('click', function () {
                 const bookId = this.getAttribute("data-id");
+                console.log(bookId);
                 removeBook(bookId);
              });
         });
@@ -56,12 +82,16 @@ function displayBooks() {
 
 displayBooks();
 
-//remove button function
+// remove button function pt.2
 function removeBook(id) {
     const index = myLibrary.findIndex(book => book.id === id);
-    myLibrary.splice(index, 1);
+    console.log(index);
+    if (index !== -1) {
+        myLibrary.splice(index, 1);
+    }
     displayBooks();
 }
+
 
 //dialog form function
 const showDialog = document.getElementById('show-dialog');
@@ -87,4 +117,4 @@ addButton.addEventListener('click', (e) => {
 
     addBookToLibrary(newBook);
     displayBooks();
-})
+});
